8912e1281b42b9669e576ffffe9bb15f613d65b5
zshumate <shumatezack@gmail.com>
2017-08-25 11:10:03 -0400

    finished collecting cities into a list of dictionaries for Project 1

--- Project1/bruteforce.py
+++ Project1/bruteforce.py
@@ -1,8 +1,18 @@
 #!/usr/bin/env python
 
 import sys
+from itertools import *
 
 def main(argv):
+    cities = []
+
+    with open(argv[1]) as f:
+        for i in xrange(7):
+            f.next()
+        for line in f:
+            words = line.split(" ")
+            cities.append({"num":words[0],"x":words[1],"y":words[2].strip()})
+
     
 
 if __name__ == "__main__":
