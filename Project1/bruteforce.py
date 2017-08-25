576fdbd76375d25da5ec0961e27fc848ea0921a9
zshumate <shumatezack@gmail.com>
2017-08-25 13:31:57 -0400

    added checks on permutations for cycles

--- Project1/bruteforce.py
+++ Project1/bruteforce.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python
 
 import sys
+import time
+import math
 from itertools import *
 
 def main(argv):
+    start = time.time()
     cities = []
+    output = []
+    num = 0
 
     with open(argv[1]) as f:
         for i in xrange(7):
@@ -12,8 +17,24 @@ def main(argv):
         for line in f:
             words = line.split(" ")
             cities.append({"num":words[0],"x":words[1],"y":words[2].strip()})
+            num += 1
 
-    
+    first = cities.pop(0)
+    num -= 1
+    for p in permutations(cities):
+        count = 0
+        distance = 0
+        while count < num:
+            # fix this if statment to properly finish the cycle
+            if(count == 0):
+                distance += math.sqrt(pow(float(first['x']) - float(p[count]['x']), 2) + pow(float(first['y']) - float(p[count]['y']), 2))
+            else:
+                distance += math.sqrt(pow(float(p[count-1]['x']) - float(p[count]['x']), 2) + pow(float(p[count-1]['y']) - float(p[count]['y']), 2))
+            output.append({"path":list(p).insert(0,first), "distance":distance}) # not working
+            count += 1
+    print(output)
+
+    print '\nThis script took ', time.time()-start, ' seconds.'
 
 if __name__ == "__main__":
     main(sys.argv)
