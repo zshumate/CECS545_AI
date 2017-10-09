a7a2eb2e43119b7fbd78ab27c32e6131d720496d
zshumate <shumatezack@gmail.com>
2017-10-09 11:44:32 -0400

    adjusted genetic algorithm for tsp to work for project, still need to add city numbers to graph, efficiency graph, and multiprocessing(?)

--- Project4/geneticAlg.py
+++ Project4/geneticAlg.py
@@ -10,8 +10,8 @@ from pyevolve import Mutators
 from pyevolve import Crossovers
 from pyevolve import Consts
 
+import matplotlib.pyplot as plt
 import sys, random
-random.seed(1024)
 from math import sqrt
 
 PIL_SUPPORT = None
@@ -22,60 +22,64 @@ try:
 except:
    PIL_SUPPORT = False
 
-
-cm     = []
 coords = []
-CITIES = 100
-WIDTH   = 1024
-HEIGHT  = 768
 LAST_SCORE = -1
+
 
 def cartesian_matrix(coords):
    """ A distance matrix """
-   matrix={}
+   matrix = {}
    for i,(x1,y1) in enumerate(coords):
       for j,(x2,y2) in enumerate(coords):
          dx, dy = x1-x2, y1-y2
-         dist=sqrt(dx*dx + dy*dy)
+         dist = sqrt(dx*dx + dy*dy)
          matrix[i,j] = dist
    return matrix
 
-def tour_length(matrix, tour):
+def tour_length(matrix, tour, num):
    """ Returns the total length of the tour """
    total = 0
    t = tour.getInternalList()
-   for i in range(CITIES):
-      j      = (i+1)%CITIES
+   for i in range(num):
+      j      = (i+1)%num
       total += matrix[t[i], t[j]]
    return total
 
 def write_tour_to_img(coords, tour, img_file):
    """ The function to plot the graph """
-   padding=20
-   coords=[(x+padding,y+padding) for (x,y) in coords]
-   maxx,maxy=0,0
-   for x,y in coords:
-      maxx, maxy = max(x,maxx), max(y,maxy)
-   maxx+=padding
-   maxy+=padding
-   img=Image.new("RGB",(int(maxx),int(maxy)),color=(255,255,255))
-   font=ImageFont.load_default()
-   d=ImageDraw.Draw(img);
-   num_cities=len(tour)
+   x, y = [], []
+   # padding=20
+   # coords=[(x+padding,y+padding) for (x,y) in coords]
+   # maxx,maxy=0,0
+   # for x,y in coords:
+   #    maxx, maxy = max(x,maxx), max(y,maxy)
+   # maxx+=padding
+   # maxy+=padding
+   # img = Image.new("RGB",(int(maxx),int(maxy)),color=(255,255,255))
+   # font=ImageFont.load_default()
+   # d=ImageDraw.Draw(img);
+   num_cities = len(tour)
    for i in range(num_cities):
-      j=(i+1)%num_cities
-      city_i=tour[i]
-      city_j=tour[j]
-      x1,y1=coords[city_i]
-      x2,y2=coords[city_j]
-      d.line((int(x1),int(y1),int(x2),int(y2)),fill=(0,0,0))
-      d.text((int(x1)+7,int(y1)-5),str(i),font=font,fill=(32,32,32))
+      city_i = tour[i]
+      a, b = coords[city_i]
+      x.append(a)
+      y.append(b)
+   city_0 = tour[0]
+   a, b = coords[city_0]
+   x.append(a)
+   y.append(b)
 
-   for x,y in coords:
-      x,y=int(x),int(y)
-      d.ellipse((x-5,y-5,x+5,y+5),outline=(0,0,0),fill=(196,196,196))
-   del d
-   img.save(img_file, "PNG")
+   plt.clf()
+   plt.plot(x,y)
+   plt.savefig(img_file)
+    #   d.line((int(x1),int(y1),int(x2),int(y2)),fill=(0,0,0))
+    #   d.text((int(x1)+7,int(y1)-5),str(i),font=font,fill=(32,32,32))
+
+   # for x,y in coords:
+   #    x,y=int(x),int(y)
+   #    d.ellipse((x-5,y-5,x+5,y+5),outline=(0,0,0),fill=(196,196,196))
+   # del d
+   # img.save(img_file, "PNG")
    print "The plot was saved into the %s file." % (img_file,)
 
 def G1DListTSPInitializator(genome, **args):
@@ -95,18 +99,28 @@ def evolve_callback(ga_engine):
       best = ga_engine.bestIndividual()
       if LAST_SCORE != best.getRawScore():
          write_tour_to_img( coords, best, "tspimg/tsp_result_%d.png" % ga_engine.getCurrentGeneration())
+        #  graph(best, "tspimg/tsp_result_%d.png" % ga_engine.getCurrentGeneration())
          LAST_SCORE = best.getRawScore()
    return False
 
 def main(argv):
-   global cm, coords, WIDTH, HEIGHT
+   global coords
+   cm     = []
+   cities = []
 
-   coords = [(random.randint(0, WIDTH), random.randint(0, HEIGHT))
-                 for i in xrange(CITIES)]
+   with open(argv[1]) as f:
+       for i in xrange(7):
+           f.next()
+       for line in f:
+           words = line.split(" ")
+           cities.append({"num":words[0],"x":words[1],"y":words[2].strip()})
+
+   coords = [(float(c['x']), float(c['y']))
+                 for c in cities]
    cm     = cartesian_matrix(coords)
    genome = G1DList.G1DList(len(coords))
 
-   genome.evaluator.set(lambda chromosome: tour_length(cm, chromosome))
+   genome.evaluator.set(lambda chromosome: tour_length(cm, chromosome, len(cities)))
    genome.crossover.set(Crossovers.G1DListCrossoverEdge)
    genome.initializator.set(G1DListTSPInitializator)
 
@@ -125,10 +139,13 @@ def main(argv):
    ga.evolve(freq_stats=500)
    best = ga.bestIndividual()
 
-   if PIL_SUPPORT:
-      write_tour_to_img(coords, best, "tsp_result.png")
-   else:
-      print "No PIL detected, cannot plot the graph !"
+   graph(coords)
+   print(best)
+
+   # if PIL_SUPPORT:
+   #    write_tour_to_img(coords, best, "tsp_result.png")
+   # else:
+   #    print "No PIL detected, cannot plot the graph !"
 
 if __name__ == "__main__":
     main(sys.argv)
